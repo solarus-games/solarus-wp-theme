7f036607cef30523379eac2862bd99a42034fbe6
Binbin <contact@zeldaforce.net>
2017-12-07 18:29:30 +0100

    Debug

--- views/front/shortcodes/games_thumbnail.php
+++ views/front/shortcodes/games_thumbnail.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <?php foreach($posts as $p):?>
-                <div class="col-lg-3">
+                <div class="col-lg-4">
                     <div class="thumbnail">
                         <a href="<?php echo get_the_permalink($p);?>" title="<?php echo Core::get_title($p);?>">
                             <?php echo get_the_post_thumbnail($p->ID);?>
