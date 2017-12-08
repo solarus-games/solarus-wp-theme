db22976f6c96bb5ac4ad27e2a351a2e0671a10f4
Binbin <contact@zeldaforce.net>
2017-12-08 08:22:41 +0100

    Jumbotron home

--- views/front/shortcodes/jumbotron.php
+++ views/front/shortcodes/jumbotron.php
@@ -1,20 +1,32 @@
 <div class="shortcode shortcode-jumbotron">
+    <div class="background"></div>
     <div class="grid"></div>
+    <div class="curves"></div>
     <div class="jumbotron">
-        <svg viewBox="0 0 1440 574" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414">
-            <g class="curve" fill="#fff" fill-opacity=".05">
-                <path d="M1440 573.417H0V435.83c46.087 18.236 98.185 31.085 158.25 35.749C516.228 499.375 896.757 25.293 1440 0v573.417z"/>
-                <path d="M1440 573.417H0v-88.22C593.208 614.335 858.302 70.795 1440 326.061v247.356z"/>
-                <path d="M1440 573.417H0v-49.316c423.696 91.601 985.817-208.322 1440-145.675v194.991z"/>
-            </g>
-        </svg>
         <div class="container">
             <div class="row">
                 <div class="col-lg-6">
                     <div class="picture"></div>
                 </div>
                 <div class="col-lg-6">
-                    <?php echo $content;?>
+                    <div class="logo"></div>
+                    <div class="content">
+                        <?php echo $content;?>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-12 text-center">
+                            <a href="#" class="btn btn-default btn-lg">
+                                <?php echo __("Overview", "solarus");?>
+                            </a>
+                            <a href="#" class="btn btn-primary btn-lg">
+                                <i class="fa fa-download"></i>
+                                <div class="text">
+                                    <?php echo __("Download", "solarus");?>
+                                    <div class="small"><?php echo __("Version 1.6.0", "solarus");?></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
